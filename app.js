6401fb4dce99867886e29b3178be510cb5e94770
qpirmez <quentin.pirmez@gmail.com>
2019-08-03 15:46:31 +0200

    Add recognizer page and move mark.js script into tagger page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var recognizerRouter = require('./routes/recognizer');
 var taggerRouter = require('./routes/tagger');
 
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tagger', taggerRouter);
+app.use('/recognizer', recognizerRouter);
 
 
 
